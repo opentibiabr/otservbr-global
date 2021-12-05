4f038a55b777eee78d4a1fd0ec3a3d81107ce1b8
Gabriel Pedro <gpedro@users.noreply.github.com>
2021-12-05 13:33:42 -0400

    Update creature.lua

--- data/events/scripts/creature.lua
+++ data/events/scripts/creature.lua
@@ -16,14 +16,6 @@ function Creature:onChangeOutfit(outfit)
 	return true
 end
 
-function Creature:onAreaCombat(tile, isAggressive)
-	return RETURNVALUE_NOERROR
-end
-
-function Creature:onTargetCombat(target)
-	return RETURNVALUE_NOERROR
-end
-
 function Creature:onHear(speaker, words, type)
 end
 
