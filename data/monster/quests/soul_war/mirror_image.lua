5f004ad186a3249f5948bd3ddfc40fed8748ed57
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-01-17 12:45:36 -0300

    Removed creature events from monsters script interface (#353)
    Scripts cannot be placed inside the monsters folder, as it is the monsters only interface, all other types of scripts need to be in the scripts folder.

--- data/monster/quests/soul_war/mirror_image.lua
+++ data/monster/quests/soul_war/mirror_image.lua
@@ -109,26 +109,3 @@ monster.events = {
 }
 
 mType:register(monster)
-
-local mirrorImageTransform = CreatureEvent("MirrorImageTransform")
-
-function mirrorImageTransform.onHealthChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType)
-	if attacker:isPlayer() then
-		local newForm = nil
-		if attacker:isSorcerer() then
-			newForm = "Sorcerer's Apparition"
-		elseif attacker:isDruid() then
-			newForm = "Druid's Apparition"
-		elseif attacker:isPaladin() then
-			newForm = "Paladin's Apparition"
-		elseif attacker:isKnight() then
-			newForm = "Knight's Apparition"
-		end
-		if newForm then
-			creature:setType(newForm)
-		end
-	end
-	return primaryDamage, primaryType, secondaryDamage, secondaryType
-end
-
-mirrorImageTransform:register()
