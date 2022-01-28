eb42abf4550cab6ec60b693f97ca3e52007eee67
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-01-28 18:35:49 -0300

    Improve familiars spells

--- data/lib/core/player.lua
+++ data/lib/core/player.lua
@@ -325,3 +325,51 @@ function Player.sendWeatherEffect(self, groundEffect, fallEffect, thunderEffect)
         end
     end
 end
+
+function Player:CreateFamiliarSpell()
+	local playerPosition = self:getPosition()
+	if not self:isPremium() then
+		playerPosition:sendMagicEffect(CONST_ME_POFF)
+		player:sendCancelMessage("You need a premium account.")
+		return false
+	end
+
+	if #self:getSummons() >= 1 then
+		self:sendCancelMessage("You can't have other summons.")
+		playerPosition:sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	local vocation = FAMILIAR_ID[self:getVocation():getBaseId()]
+	local familiarName
+
+	if vocation then
+		familiarName = vocation.name
+	end
+
+	if not familiarName then
+		self:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+		playerPosition:sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	local myFamiliar = Game.createMonster(familiarName, playerPosition, true, false, self)
+	if not myFamiliar then
+		self:sendCancelMessage(RETURNVALUE_NOTENOUGHROOM)
+		playerPosition:sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	myFamiliar:setOutfit({lookType = self:getFamiliarLooktype()})
+	myFamiliar:registerEvent("FamiliarDeath")
+	local deltaSpeed = math.max(self:getSpeed() - myFamiliar:getBaseSpeed(), 0)
+	myFamiliar:changeSpeed(deltaSpeed)
+	playerPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+	myFamiliar:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	self:setStorageValue(Storage.FamiliarSummon, os.time() + 15*60) -- 15 minutes from now
+	addEvent(removeFamiliar, 15*60*1000, myFamiliar:getId(), self:getId())
+	for sendMessage = 1, #FAMILIAR_TIMER do
+		self:setStorageValue(FAMILIAR_TIMER[sendMessage].storage,addEvent(sendMessageFunction, (15*60-FAMILIAR_TIMER[sendMessage].countdown)*1000, self:getId(),FAMILIAR_TIMER[sendMessage].message))
+	end
+	return true
+end
