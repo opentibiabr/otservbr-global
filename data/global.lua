0138ce0e190467c14470ae7fb765caa67e250f8f
EeFo17 <97588773+EeFo17@users.noreply.github.com>
2022-02-07 18:09:41 +0100

    Update global.lua

--- data/global.lua
+++ data/global.lua
@@ -226,6 +226,51 @@ function addStamina(playerId, ...)
 	end
 	return false
 end
+	
+	function getPlayerSpouse(id)
+    local resultQuery = db.storeQuery("SELECT `marriage_spouse` FROM `players` WHERE `id` = " .. db.escapeString(id))
+    if resultQuery ~= false then
+        local ret = result.getDataInt(resultQuery, "marriage_spouse")
+        result.free(resultQuery)
+        return ret
+    end
+    return -1
+end
+
+function setPlayerSpouse(id, val)
+    db.query("UPDATE `players` SET `marriage_spouse` = " .. val .. " WHERE `id` = " .. id)
+end
+
+function getPlayerMarriageStatus(id)
+    local resultQuery = db.storeQuery("SELECT `marriage_status` FROM `players` WHERE `id` = " .. db.escapeString(id))
+    if resultQuery ~= false then
+        local ret = result.getDataInt(resultQuery, "marriage_status")
+        result.free(resultQuery)
+        return ret
+    end
+    return -1
+end
+
+function setPlayerMarriageStatus(id, val)
+    db.query("UPDATE `players` SET `marriage_status` = " .. val .. " WHERE `id` = " .. id)
+end
+
+function Player:getMarriageDescription(thing)
+    local descr = ""
+    if getPlayerMarriageStatus(thing:getGuid()) == MARRIED_STATUS then
+        playerSpouse = getPlayerSpouse(thing:getGuid())
+        if self == thing then
+            descr = descr .. " You are "
+        elseif thing:getSex() == PLAYERSEX_FEMALE then
+            descr = descr .. " She is "
+        else
+            descr = descr .. " He is "
+        end
+        descr = descr .. "married to " .. getPlayerNameById(playerSpouse) .. '.'
+    end
+    return descr
+end
+
 
 -- Exercise Training
 onExerciseTraining = {}
