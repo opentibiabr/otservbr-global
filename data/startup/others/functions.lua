8d7504ba823c7a0715f9ff741e05a7d671d0c49c
Marcos <66353315+marcosvf132@users.noreply.github.com>
2022-02-09 03:00:45 -0300

    [Fix] Action loader bug.

--- data/startup/others/functions.lua
+++ data/startup/others/functions.lua
@@ -14,7 +14,7 @@ function loadLuaMapAction(tablename)
 					break
 				end
 
-				if tile:getItemCountById(value.itemId) > 1 then
+				if value.itemId ~= false and tile:getItemCountById(value.itemId) == 1 then
 					item = tile:getItemById(value.itemId)
 				end
 				
