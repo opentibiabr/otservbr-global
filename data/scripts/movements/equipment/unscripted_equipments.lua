767f9e05ad66e0aede68476ccc74d9412cface3b
painer15999 <50599143+painer15999@users.noreply.github.com>
2022-02-16 12:57:18 -0300

    Fix lion shield on unscripted equipments table (#435)
    * add lion-shield unscripted_weapons.
    
    * converti id and Register the lion shield unscripted weapons table
    
    * Fix lion shield on unscripted equipments
    
    Co-authored-by: Majesty <32709570+MajestyOTBR@users.noreply.github.com>

--- data/scripts/movements/equipment/unscripted_equipments.lua
+++ data/scripts/movements/equipment/unscripted_equipments.lua
@@ -806,6 +806,23 @@ local items = {
 		slot = "hand"
 	},
 	{
+		-- lion shield
+		itemid = 34154,
+		type = "equip",
+		slot = "hand",
+		level = 250,
+		vocation = {
+			{"Knight", true},
+			{"Elite Knight"}
+		}
+	},
+	{
+		-- lion shield
+		itemid = 34154,
+		type = "deequip",
+		slot = "hand"
+	},
+	{
 		-- lion spellbook
 		itemid = 34153,
 		type = "deequip",
@@ -874,17 +891,7 @@ local items = {
 		itemid = 34150,
 		type = "deequip",
 		slot = "hand"
-	}, -- lion shield
-	{
-		itemid = 38989,
-		type = "equip",
-		slot = "shield",
-		level = 250,
-		vocation = {
-			{"Knight", true},
-			{"Elite Knight"}
-		}
-	}, -- lion shield
+	},
 	{
 		-- soulbastion shield
 		itemid = 34099,
