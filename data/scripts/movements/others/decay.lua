b0104452422cf0924087cdd324c18079aec8dddb
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-02-22 12:40:17 -0300

    [Npc] - Fix script decay.lua for work only for players (#442)

--- data/scripts/movements/others/decay.lua
+++ data/scripts/movements/others/decay.lua
@@ -1,6 +1,11 @@
 local decay = MoveEvent()
 
 function decay.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return false
+	end
+
 	item:transform(item.itemid + 1)
 	item:decay()
 	return true
