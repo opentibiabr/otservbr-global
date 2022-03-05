28aa1f084fa85f7a3fefbd0696e730cb0b28c19d
Marcos <66353315+marcosvf132@users.noreply.github.com>
2022-03-05 09:10:21 -0300

    Improvements and fixes

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -10,13 +10,23 @@ function Monster:onDropLoot(corpse)
 	end
 
 	local player = Player(corpse:getCorpseOwner())
-	local mType = self:getType()
 	if not player or player:getStamina() > 840 then
 		local monsterLoot = mType:getLoot()
+		local preyChanceBoost = 100
+		local charmBonus = false
+		if player and mType and mType:raceId() > 0 then
+			preyChanceBoost = player:getPreyLootPercentage(mType:raceId())
+			local charm = player:getCharmMonsterType(CHARM_GUT)
+			if charm and charm:raceId() == mType:raceId() then
+				charmBonus = true
+			end
+		end
+
+
 		for i = 1, #monsterLoot do
-			local item = corpse:createLootItem(monsterLoot[i], player, self)
+			local item = corpse:createLootItem(monsterLoot[i], charmBonus, preyChanceBoost)
 			if self:getName():lower() == (Game.getBoostedCreature()):lower() then
-				local itemBoosted = corpse:createLootItem(monsterLoot[i], player, self)
+				local itemBoosted = corpse:createLootItem(monsterLoot[i], charmBonus, preyChanceBoost)
 			end
 			if not item then
 				Spdlog.warn(string.format("[Monster:onDropLoot] - Could not add loot item to monster: %s, from corpse id: %d.", self:getName(), corpse:getId()))
@@ -29,6 +39,12 @@ function Monster:onDropLoot(corpse)
 				 text = ("Loot of %s: %s (boosted loot)"):format(mType:getNameDescription(), corpse:getContentDescription())
 			else
 				 text = ("Loot of %s: %s"):format(mType:getNameDescription(), corpse:getContentDescription())			
+			end
+			if preyChanceBoost ~= 100 then
+				text = text .. " (active prey bonus)"
+			end
+			if charmBonus then
+				text = text .. " (active charm bonus)"
 			end
 			local party = player:getParty()
 			if party then
