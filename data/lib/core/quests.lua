bfe481dd9093539c91b1d12bad366e5963312933
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-03-09 08:50:48 -0300

    Move to the correct place

--- data/lib/core/quests.lua
+++ data/lib/core/quests.lua
@@ -113,6 +113,52 @@ if not Quests then
 				}
 			}
 		},
+		[3] = {
+			name = "Spike Task",
+			startStorageId = Storage.SpikeTaskQuest.QuestLine,
+			startStorageValue = 1,
+			missions = {
+				[1] = {
+					name = "First Task",
+					storageId = Storage.SpikeTaskQuest.Gnomilly,
+					missionId = 1021,
+					startValue = 0,
+					endValue = 100,
+					description = function(player)
+						return string.format(
+							"You have %d points of task. You need 100 points to take Cave Explorer outfit.",
+							(math.max(player:getStorageValue(Storage.SpikeTaskQuest.Gnomilly), 0))
+						)
+					end
+				},
+				[2] = {
+					name = "Second Task",
+					storageId = Storage.SpikeTaskQuest.Gnombold.Points,
+					missionId = 1022,
+					startValue = 0,
+					endValue = 100,
+					description = function(player)
+						return string.format(
+							"You have %d points of task. You need 100 points to take first addon.",
+							(math.max(player:getStorageValue(Storage.SpikeTaskQuest.Gnombold.Points), 0))
+						)
+					end
+				},
+				[3] = {
+					name = "Third Task",
+					storageId = Storage.SpikeTaskQuest.Gnomargery.Points,
+					missionId = 1023,
+					startValue = 0,
+					endValue = 100,
+					description = function(player)
+						return string.format(
+							"You have %d points of task. You need 100 points to take second addon.",
+							(math.max(player:getStorageValue(Storage.SpikeTaskQuest.Gnomargery.Points), 0))
+						)
+					end
+				}
+			}
+		},
 		[4] = {
 			name = "A Father\'s Burden",
 			startStorageId = Storage.FathersBurden.QuestLog,
@@ -5766,52 +5812,6 @@ if not Quests then
 				}
 			}
 		},
-		[3] = {
-			name = "Spike Task",
-			startStorageId = Storage.SpikeTaskQuest.QuestLine,
-			startStorageValue = 1,
-			missions = {
-				[1] = {
-					name = "First Task",
-					storageId = Storage.SpikeTaskQuest.Gnomilly,
-					missionId = 1021,
-					startValue = 0,
-					endValue = 100,
-					description = function(player)
-						return string.format(
-							"You have %d points of task. You need 100 points to take Cave Explorer outfit.",
-							(math.max(player:getStorageValue(Storage.SpikeTaskQuest.Gnomilly), 0))
-						)
-					end
-				},
-				[2] = {
-					name = "Second Task",
-					storageId = Storage.SpikeTaskQuest.Gnombold.Points,
-					missionId = 1022,
-					startValue = 0,
-					endValue = 100,
-					description = function(player)
-						return string.format(
-							"You have %d points of task. You need 100 points to take first addon.",
-							(math.max(player:getStorageValue(Storage.SpikeTaskQuest.Gnombold.Points), 0))
-						)
-					end
-				},
-				[3] = {
-					name = "Third Task",
-					storageId = Storage.SpikeTaskQuest.Gnomargery.Points,
-					missionId = 1023,
-					startValue = 0,
-					endValue = 100,
-					description = function(player)
-						return string.format(
-							"You have %d points of task. You need 100 points to take second addon.",
-							(math.max(player:getStorageValue(Storage.SpikeTaskQuest.Gnomargery.Points), 0))
-						)
-					end
-				}
-			}
-		}
 	}
 end
 
