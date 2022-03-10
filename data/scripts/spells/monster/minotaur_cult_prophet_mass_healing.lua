4621e60197e405909c391c43330f09cf84092fcd
Wojciech Dybikowski <dybik08@gmail.com>
2022-03-10 19:14:32 +0100

    fixed issue with minotaur cult prophet not healing itself

--- data/scripts/spells/monster/minotaur_cult_prophet_mass_healing.lua
+++ data/scripts/spells/monster/minotaur_cult_prophet_mass_healing.lua
@@ -7,13 +7,15 @@ local monsters = {
 	"minotaur cult zealot",
 }
 
+local healingValue = math.random(200, 350)
+
 function onTargetTile(creature, pos)
 	local tile = Tile(pos)
 	if tile then
 		local target = tile:getTopCreature()
 		if target and target ~= creature then
 			if table.contains(monsters, target:getName():lower()) then
-				target:addHealth(math.random(200, 350))
+				target:addHealth(healingValue)
 			end
 		end
 	end
@@ -26,6 +28,7 @@ combat:setCallback(CALLBACK_PARAM_TARGETTILE, "onTargetTile")
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
+	creature:addHealth(healingValue) -- otherwise minotaur cult didnt heal itself, only allies
 	return combat:execute(creature, variant)
 end
 
