ecf0af8b0f2882af8f0e8650eaeb75c00727472e
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-04-24 10:53:41 -0300

    Missing

--- data/global.lua
+++ data/global.lua
@@ -159,6 +159,18 @@ if not playerDelayPotion then
 	playerDelayPotion = {}
 end
 
+-- for use of: data\scripts\globalevents\customs\save_interval.lua
+SAVE_INTERVAL_TYPE = configManager.getString(configKeys.SAVE_INTERVAL_TYPE)
+SAVE_INTERVAL_CONFIG_TIME = configManager.getNumber(configKeys.SAVE_INTERVAL_TIME)
+SAVE_INTERVAL_TIME = 0
+if SAVE_INTERVAL_TYPE == "second" then
+	SAVE_INTERVAL_TIME = 1000
+elseif SAVE_INTERVAL_TYPE == "minute" then
+	SAVE_INTERVAL_TIME = 60 * 1000
+elseif SAVE_INTERVAL_TYPE == "hour" then
+	SAVE_INTERVAL_TIME = 60 * 60 * 1000
+end
+
 -- Increase Stamina when Attacking Trainer
 staminaBonus = {
 	target = 'Training Machine',
