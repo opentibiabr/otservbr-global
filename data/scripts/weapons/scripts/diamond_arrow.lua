c6253ecad491f52c2029143978cb650babde2b48
willianhumilde <will99jj@gmail.com>
2022-05-16 15:32:53 -0300

    Update diamond_arrow.lua

--- data/scripts/weapons/scripts/diamond_arrow.lua
+++ data/scripts/weapons/scripts/diamond_arrow.lua
@@ -1,9 +1,9 @@
 local area = createCombatArea({
- 	{0, 1, 1, 1, 0},
- 	{1, 1, 1, 1, 1},
- 	{1, 1, 3, 1, 1},
-	{1, 1, 1, 1, 1},
-	{0, 1, 1, 1, 0},
+     {0, 1, 1, 1, 0},
+     {1, 1, 1, 1, 1},
+     {1, 1, 3, 1, 1},
+     {1, 1, 1, 1, 1},
+     {0, 1, 1, 1, 0},
  })
 
 local combat = Combat()
@@ -14,7 +14,7 @@ combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
 function onGetFormulaValues(player, skill, attack, factor)
     local distanceSkill = player:getEffectiveSkillLevel(SKILL_DISTANCE)
     local min = (player:getLevel() / 5)
-    local max = (0.09 * 1 * distanceSkill * 37) + (player:getLevel() / 5)
+    local max = (0.09 * factor) * distanceSkill * 37 + (player:getLevel() / 5)
     return -min, -max
 end
 
@@ -24,7 +24,7 @@ combat:setArea(area)
 local diamondArrow = Weapon(WEAPON_AMMO)
 
 function diamondArrow.onUseWeapon(player, variant)
-	return combat:execute(player, variant)
+    return combat:execute(player, variant)
 end
 
 diamondArrow:id(25757)
