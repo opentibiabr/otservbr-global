3ff747277688c19c6b42f1423530bbe74980d3ad
rigis1 <rigis@o2.pl>
2022-05-31 17:58:58 +0200

    fix corpse id

--- data/monster/mammals/ghost_wolf.lua
+++ data/monster/mammals/ghost_wolf.lua
@@ -29,7 +29,7 @@ monster.Bestiary = {
 monster.health = 160
 monster.maxHealth = 160
 monster.race = "blood"
-monster.corpse = 6009
+monster.corpse = 22078
 monster.speed = 270
 monster.manaCost = 420
 
