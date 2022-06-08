46d07045d505fc341b4a76daf6d0f3123032c8f0
sircurse <anderson@lalumail.com>
2022-06-08 14:53:55 +0200

    Quests storage restructuring
    Quests storage restructuring

--- data/startup/others/load.lua
+++ data/startup/others/load.lua
@@ -1,3 +1,4 @@
 dofile('data/startup/others/bitwise_flags.lua')
 dofile('data/startup/others/functions.lua')
 dofile('data/startup/others/hireling_lib.lua')
+dofile('data/startup/others/quest_keys_update.lua')
