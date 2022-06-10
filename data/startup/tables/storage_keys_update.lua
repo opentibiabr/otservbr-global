3e3d453fcb3e270a8eb0b984ccf1e054ecbfad8a
sircurse <anderson@lalumail.com>
2022-06-10 19:04:05 +0200

    Update storage_keys_update.lua

--- data/startup/tables/storage_keys_update.lua
+++ data/startup/tables/storage_keys_update.lua
@@ -1,6 +1,6 @@
 QuestKeysUpdate = {
 	[0] = {latest = 1}, -- Increase +1 every time this above table suffer a modification
-	-- Add a new table to the bottow of this array with the following numver versioning.
+	-- Add a new table to the bottow of this array with the following number versioning.
 	[1] = {
 		-- Deeper Fibula
 		{old = 10001, new = 70001}, -- RewardTowerShield
@@ -8,39 +8,40 @@ QuestKeysUpdate = {
 		{old = 10003, new = 70003}, -- RewardDwarvenRing
 		{old = 10004, new = 70004}, -- RewardElvenAmulet
 		{old = 10005, new = 70005}, -- RewardKnightAxe
-
 		-- Ornamented Shield
 		{old = 10006, new = 70006}, -- Bag
 		{old = 10007, new = 70007}, -- RedBag
-
 		-- Short Sword
 		{old = 10008, new = 70008}, -- Book
-
 		-- Thais Lighthouse
 		{old = 10009, new = 70009}, -- BattleHammer
 		{old = 10010, new = 70010}, -- DarkShield
-
 		-- Studded Shield
 		{old = 10011, new = 70021}, -- BananaFree
 		{old = 10012, new = 70022}, -- BananaPremium
-
 		-- Emperor's Cookies
 		{old = 10016, new = 70031}, -- Bag With Cookies and Prison Key
-
 		-- Explorer Brooch
 		{old = 10017, new = 70032}, -- Brooch
-
 		-- Orc Fortress
 		{old = 10018, new = 70033}, -- Knight Axe
 		{old = 10019, new = 70034}, -- Knight Armor
 		{old = 10020, new = 70035}, -- Fire Sword
-
 		-- Panpipe
 		{old = 10022, new = 70036}, -- Panpipe
-
 		-- Draconia Quest
 		{old = 10023, new = 70051}, -- BagWithWeapons
 		{old = 10024, new = 70052}, -- BagWithAmulets
 
 	},
+--[[
+	[2] = { -- New table version, allows the updated servers apply the changes to the only new revised quest.
+		-- Example of new quest revised to be migrated
+		{old = 10001, new = 80001}, -- Example
+		{old = 10002, new = 80002}, -- Example
+		{old = 10003, new = 80003}, -- Example
+		{old = 10004, new = 80004}, -- Example
+		{old = 10005, new = 80005}, -- Example
+	},
+]]--
 }
