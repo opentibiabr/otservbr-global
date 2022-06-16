70e48d20fe5ae2e744b24ab0bea7f34c7ea332ac
travisani <gabrielt19@live.com>
2022-06-16 18:40:38 -0300

    Fix Golden/Royal Outfit Memorial
    Like Global Tibia.

--- data/scripts/actions/other/golden_outfit_memorial.lua
+++ data/scripts/actions/other/golden_outfit_memorial.lua
@@ -1,25 +1,66 @@
-local storage = Storage.OutfitQuest.GoldenOutfit
+local updateInterval = 5 * 60 -- The Memorial will be updated in 5 minutes
+
+local goldenOutfitCache
+local lastUpdated = 0
+local function updateGoldenOutfitCache()
+	if os.time() < lastUpdated + updateInterval then
+		return
+	end
+
+	goldenOutfitCache = {[1] = {}, [2] = {}, [3] = {}}
+
+	local resultId = db.storeQuery("SELECT `name`, `value` FROM `player_storage` INNER JOIN `players` as `p` ON `p`.`id` = `player_id` WHERE `key` = " .. Storage.OutfitQuest.GoldenOutfit .. " AND `value` >= 1;")
+	if not resultId then
+		result.free(resultId)
+		lastUpdated = os.time()
+		return
+	end
+
+	repeat
+		local addons = result.getNumber(resultId, "value")
+		local name = result.getString(resultId, "name")
+		table.insert(goldenOutfitCache[addons], name)
+	until not result.next(resultId)
+	result.free(resultId)
+
+	lastUpdated = os.time()
+end
+
+local function displayMemorialUI(player)
+	local response = NetworkMessage()
+	response:addByte(0xB0)
+
+	local price = 500000000
+	response:addU32(price)
+	price = price + 250000000
+	response:addU32(price)
+	price = price + 250000000
+	response:addU32(price)
+
+	for i = 1, 3 do
+		response:addU16(#goldenOutfitCache[i])
+		for j = 1, #goldenOutfitCache[i] do
+			response:addString(goldenOutfitCache[i][j])
+		end
+	end
+
+	for i = 1, 3 do
+		response:addU16(0) -- price in silver tokens
+		response:addU16(0) -- price in golden tokens
+	end
+
+	for i = 1, 3 do
+		response:addU16(0) -- list of spenders
+	end
+
+	response:sendToPlayer(player)
+end
 
 local goldenOutfitMemorial = Action()
-
 function goldenOutfitMemorial.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-    local resultId = db.storeQuery("SELECT `player_id` FROM `player_storage` WHERE `key` = " .. storage .. " AND `value` >= 3;")
-    if resultId ~= false then
-        local message = "The following characters have spent a fortune on a Golden Outfit:\n\nFull Outfit for 1,000,000,000 gold:\n\n"
-        repeat
-            local resultId2 = db.storeQuery("SELECT `name` FROM `players` WHERE `id` = " .. result.getDataInt(resultId, "player_id") .. ";")
-            if resultId2 ~= false then
-                local playerName = result.getString(resultId2, "name")
-                message = ""..message.."- ".. playerName .."\n"
-                result.free(resultId2)
-            end
-        until not result.next(resultId)
-        result.free(resultId)
-        player:showTextDialog(item.itemid, message)
-    else
-        player:showTextDialog(item.itemid, "The Golden Outfit has not been acquired by anyone yet.")
-    end
-    return true
+	updateGoldenOutfitCache()
+	displayMemorialUI(player)
+	return true
 end
 
 goldenOutfitMemorial:id(31518, 31519, 31520, 31521, 31522, 31523)
