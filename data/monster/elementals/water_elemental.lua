d3de4a5bbdf82b1ae3704d0ab27e36f8e71c9b8e
rigis1 <rigis@o2.pl>
2022-06-21 14:31:42 +0200

    fix loot and armor

--- data/monster/elementals/water_elemental.lua
+++ data/monster/elementals/water_elemental.lua
@@ -79,20 +79,17 @@ monster.voices = {
 }
 
 monster.loot = {
-	{name = "small diamond", chance = 1000},
-	{name = "small sapphire", chance = 1000},
-	{name = "gold coin", chance = 50000, maxCount = 100},
-	{name = "small emerald", chance = 1000, maxCount = 2},
-	{name = "platinum coin", chance = 10000},
+	{name = "small diamond", chance = 2500},
+	{name = "gold coin", chance = 100000, maxCount = 100},
+	{name = "small emerald", chance = 2500, maxCount = 2},
+	{name = "platinum coin", chance = 25000},
 	{id = 3051, chance = 950}, -- energy ring
 	{id = 3052, chance = 930}, -- life ring
 	{id = 3578, chance = 20000}, -- fish
 	{name = "rainbow trout", chance = 940},
 	{name = "green perch", chance = 1050},
 	{name = "strong health potion", chance = 10000},
-	{name = "strong mana potion", chance = 10000},
-	{id = 281, chance = 800}, -- giant shimmering pearl (green)
-	{id = 282, chance = 800} -- giant shimmering pearl (brown)
+	{name = "strong mana potion", chance = 10000}
 }
 
 monster.attacks = {
@@ -105,7 +102,7 @@ monster.attacks = {
 
 monster.defenses = {
 	defense = 20,
-	armor = 20,
+	armor = 37,
 	{name ="combat", interval = 2000, chance = 5, type = COMBAT_HEALING, minDamage = 50, maxDamage = 80, effect = CONST_ME_MAGIC_BLUE, target = false},
 	{name ="speed", interval = 2000, chance = 15, speedChange = 300, effect = CONST_ME_MAGIC_RED, target = false, duration = 5000}
 }
