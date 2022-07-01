8d3ef6cbba922cf4a9a67f653222a08c981c548f
sircurse <anderson@lalumail.com>
2022-07-01 16:42:45 +0200

    Update storage_keys_update.lua

--- data/startup/tables/storage_keys_update.lua
+++ data/startup/tables/storage_keys_update.lua
@@ -1,5 +1,5 @@
 QuestKeysUpdate = {
-	[0] = {latest = 3}, -- Increase +1 every time this below table suffer a modification
+	[0] = {latest = 4}, -- Increase +1 every time this below table suffer a modification
 	-- Add a new table to the bottow of this array with the following number versioning.
 	[1] = {
 		-- Deeper Fibula
