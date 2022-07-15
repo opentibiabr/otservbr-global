2940d6e4859f1183e4b1dd046d2eef9002c6bc0b
Bjunky <108951599+Bjunken@users.noreply.github.com>
2022-07-15 20:26:07 +0200

    Fix nightfiend corpse id and loot, carpet npcs keyword (#711)

--- data/monster/demons/nightfiend.lua
+++ data/monster/demons/nightfiend.lua
@@ -29,7 +29,7 @@ monster.Bestiary = {
 monster.health = 2700
 monster.maxHealth = 2700
 monster.race = "blood"
-monster.corpse = 3580
+monster.corpse = 18952
 monster.speed = 224
 monster.manaCost = 0
 
@@ -75,6 +75,18 @@ monster.voices = {
 }
 
 monster.loot = {
+	{name = "platinum coin", chance = 50000, maxCount = 1},
+	{name = "gold coin", chance = 50000, maxCount = 148},
+	{id = 3030, chance = 1052, maxCount = 3},
+	{name = "tooth file", chance = 564},
+	{name = "blood preservation", chance = 1000},
+	{name = "emerald bangle", chance = 120},
+	{name = "vampire teeth", chance = 10000},
+	{name = "vampire shield", chance = 50},
+	{name = "strong health potion", chance = 4761},
+	{name = "strong mana potion", chance = 5000},
+	{id = 3039, chance = 55}, -- red gem
+	{id = 3098, chance = 1000} -- ring of healing
 }
 
 monster.attacks = {
