024a172a2e1369fad94b502f94e67cbb4e5559a0
rigis1 <rigis@o2.pl>
2022-07-16 11:47:14 +0200

    Fix Wrath_of_the_Emperor_Quest
    fixed Mission 01: Catering the Lion's Den

--- data/scripts/movements/quests/wrath_of_the_emperor/crate.lua
+++ data/scripts/movements/quests/wrath_of_the_emperor/crate.lua
@@ -1,58 +1,72 @@
-function catchPlayer(cid)
-	local player = Player(cid)
-	player:setStorageValue(Storage.WrathoftheEmperor.GuardcaughtYou, 1)
-	player:setStorageValue(Storage.WrathoftheEmperor.CrateStatus, 0)
+function catchPlayer(player)
+	player:removeCondition(CONDITION_OUTFIT)
+	player:removeItem(11328, 1)
+	player:setStorageValue(Storage.WrathoftheEmperor.GuardcaughtYou, player:getStorageValue(Storage.WrathoftheEmperor.GuardcaughtYou) + 1)
 	player:teleportTo({x = 33361, y = 31206, z = 8}, false)
-	player:say("The guards have spotted you. You were forcibly dragged into a small cell. \z
-		It looks like you need to build another disguise.", TALKTYPE_MONSTER_SAY)
+	if player:getStorageValue(Storage.WrathoftheEmperor.GuardcaughtYou) == 1 then 
+		player:say("The guards have spotted you. You were forcibly dragged into a small cell. It looks like you need to build another disguise.", TALKTYPE_MONSTER_SAY)
+	end
 	return true
 end
 
+local trapDoors =
+{
+	{doorPos = Position(33385, 31139, 8)},
+	{doorPos = Position(33385, 31134, 8)},
+	{doorPos = Position(33385, 31126, 8)},
+	{doorPos = Position(33385, 31119, 8)},
+	{doorPos = Position(33385, 31118, 8)},
+	{doorPos = Position(33380, 31093, 8)},
+	{doorPos = Position(33380, 31085, 8)}
+}
+
 local crate = MoveEvent()
 
-function crate.onStepIn(creature, item, position, fromPosition)
-	local player = creature:getPlayer()
+function crate.onStepIn(player, item, position, fromPosition, toPosition)
 	if not player then
 		return true
 	end
-
-	local playerId = player:getId()
-	if item.actionid == 8015 then
-		player:say("You hear guards moving behind doors in the distance. \z
-			If you have any sort of disguise with you, this is the moment to use it.", TALKTYPE_MONSTER_SAY)
-	elseif item.actionid == 8016 then
-		if Tile(Position(player:getPosition().y < 31094 and 33080
-		or 33385, player:getPosition().y, 8)):getItemById(11257) then
-			catchPlayer(playerId)
-		end
-	elseif item.actionid == 8017 or item.actionid == 32362 or item.itemid == 10480 then
-		catchPlayer(playerId)
-	elseif item.actionid == 8018 then
+	-- if item.actionid == 8015 then
+		-- player:say("You hear guards moving behind doors in the distance. \z
+			-- If you have any sort of disguise with you, this is the moment to use it.", TALKTYPE_MONSTER_SAY)
+	if item.actionid == 50372 then
+		player:setStorageValue(Storage.WrathoftheEmperor.GuardcaughtYou, 0)		
+		catchPlayer(player)
+	elseif item.actionid == 50369 then
 		if Game.getStorageValue(GlobalStorage.WrathOfTheEmperor.Light01) ~= 1 then
-			catchPlayer(playerId)
+			catchPlayer(player)
 		end
-	elseif item.actionid == 8019 then
+	elseif item.actionid == 50370 then
 		if Game.getStorageValue(GlobalStorage.WrathOfTheEmperor.Light02) ~= 1 then
-			catchPlayer(playerId)
+			catchPlayer(player)
 		end
-	elseif item.actionid == 8020 then
+	elseif item.actionid == 50371 then
 		if Game.getStorageValue(GlobalStorage.WrathOfTheEmperor.Light03) ~= 1 then
-			catchPlayer(playerId)
+			catchPlayer(player)
 		end
-	elseif item.actionid == 8021 then
-		player:say("Guards heavily patrol this area. \z
-			Try to stay hidden and do not draw any attention to yourself by trying to attack.", TALKTYPE_MONSTER_SAY)
-	elseif item.actionid == 8022 then
-		if player:getStorageValue(Storage.WrathoftheEmperor.CrateStatus) ~= 1 then
-			catchPlayer(playerId)
+	elseif item.actionid == 50368 and player:getStorageValue(Storage.WrathoftheEmperor.Message) ~= 1 then
+		player:say("Guards heavily patrol this area. Try to stay hidden and do not draw any attention to yourself by trying to attack.", TALKTYPE_MONSTER_SAY)
+		player:setStorageValue(Storage.WrathoftheEmperor.Message, 1)
+	elseif item.actionid == 50373 then
+		if player:removeCondition(CONDITION_OUTFIT) then
+			player:say("On reaching the watchtower you remove your temporary disguise.", TALKTYPE_MONSTER_SAY)
 		end
-	elseif item.actionid == 8023 then
-		player:setStorageValue(Storage.WrathoftheEmperor.CrateStatus, 0)
-		player:setOutfit({lookTypeEx = 11540}, 1)
+	elseif item.actionid == 50367 then
+		if player:getOutfit().lookTypeEx ~= 11328 then
+			return catchPlayer(player)
+		end
+		for i = 1, #trapDoors do
+			local pos = trapDoors[i].doorPos
+			if isInArray({Position(pos.x+1, pos.y, 8), Position(pos.x+2, pos.y, 8), Position(pos.x+3, pos.y, 8)}, player:getPosition()) and Tile(pos):getItemById(11257) then
+				catchPlayer(player)
+			elseif isInArray({Position(pos.x+1, pos.y, 8), Position(pos.x+2, pos.y, 8), Position(pos.x+3, pos.y, 8)}, player:getPosition()) and Tile(pos):getItemById(11258) then
+				return true
+			end
+		end
 	end
 	return true
 end
 
 crate:type("stepin")
-crate:aid(8015, 8016, 8017, 8018, 8019, 8020, 8021, 8022, 8023, 32362)
+crate:aid(50367, 50368, 50369, 50370, 50371, 50372, 50373)
 crate:register()
