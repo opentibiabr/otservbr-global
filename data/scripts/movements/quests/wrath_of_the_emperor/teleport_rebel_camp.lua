eadeddef985fee20e0951fe261887fdca35ff3ce
rigis1 <rigis@o2.pl>
2022-07-17 14:56:58 +0200

    Update teleport_rebel_camp.lua

--- data/scripts/movements/quests/wrath_of_the_emperor/teleport_rebel_camp.lua
+++ data/scripts/movements/quests/wrath_of_the_emperor/teleport_rebel_camp.lua
@@ -1,4 +1,4 @@
-local config = 
+local config =
 {
 	[35004] = {Access = Storage.WrathoftheEmperor.TeleportAccess.Rebel, teleportPos = Position(33136, 31248, 6), destinationA = Position(33211, 31065, 9), destinationB = Position(33138, 31248, 6)},
 	[35005] = {Access = Storage.WrathoftheEmperor.TeleportAccess.Zlak, teleportPos = Position(33216, 31067, 9), destinationA = Position(33078, 31219, 8), destinationB = Position(33216, 31069, 9)}
@@ -13,7 +13,7 @@ function teleport.onStepIn(player, item, position, fromPosition)
 	for i = 35004, 35005 do
 		if item.actionid == i then
 			if player:getStorageValue(config[i].Access) == 1 then
-				if position == config[i].teleportPos then 
+				if position == config[i].teleportPos then
 					player:teleportTo(config[i].destinationA)
 					config[i].destinationA:sendMagicEffect(CONST_ME_TELEPORT)
 				else
