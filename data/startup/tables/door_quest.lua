7693a374c7d8d3c25adc55abd2a2b03741eb992f
rigis1 <rigis@o2.pl>
2022-07-17 16:47:20 +0200

    fix quest doors
    You can now be able to go outside main building by starting mission 07 A Noble Cause

--- data/startup/tables/door_quest.lua
+++ data/startup/tables/door_quest.lua
@@ -755,7 +755,12 @@ QuestDoorAction = {
 		itemPos = {
 			{x = 33080, y = 31215, z = 7},
 			{x = 33086, y = 31199, z = 7},
-			{x = 33090, y = 31190, z = 7},
+			{x = 33090, y = 31190, z = 7}
+		}
+	},
+	[Storage.WrathoftheEmperor.Mission07] = {
+		itemId = false,
+		itemPos = {
 			{x = 33073, y = 31170, z = 7},
 			{x = 33074, y = 31170, z = 7}
 		}
