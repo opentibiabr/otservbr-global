1a620b2dc0099039489ce6532a5243e654de7372
rigis1 <rigis@o2.pl>
2022-07-20 22:37:13 +0200

    fixed levers
    walls are created and removed by pull the levers

--- data/scripts/actions/quests/the_hidden_city_of_beregar/bargate_lever.lua
+++ data/scripts/actions/quests/the_hidden_city_of_beregar/bargate_lever.lua
@@ -1,49 +1,31 @@
--- NEED REVIEW
 local config = {
-	gatePositions = {
-		Position(32569, 31421, 9),
-		Position(32569, 31422, 9)
-	},
-	leverPositions = {
-		Position(32568, 31421, 9),
-		Position(32570, 31421, 9)
-	},
-	removeCreaturePosition = Position(32568, 31422, 9),
-	wallID = 1608,
-	wallID2 = 1613,
-	tileID = 501
+	{gatePosX = Position(32569, 31421, 9), gatePosY = Position(32569, 31422, 9), tileIdX = 501, tileIdY = 502},
+	{gatePosX = Position(32600, 31421, 9), gatePosY = Position(32600, 31422, 9), tileIdX = 502, tileIdY = 501}
 }
+
+local function wall(i)
+	if Tile(config[i].gatePosX):getItemById(1613) then
+		Tile(config[i].gatePosX):getItemById(1613):remove(1613)
+		Tile(config[i].gatePosX):getItemById(1608):remove(1608)
+		Tile(config[i].gatePosY):getItemById(1613):remove(1613)
+		Tile(config[i].gatePosY):getItemById(1608):remove(1608)
+	else
+		Game.createItem(1613, 1, config[i].gatePosX)
+		Game.createItem(1608, 1, config[i].gatePosX)
+		Game.createItem(1613, 1, config[i].gatePosY)
+		Game.createItem(1608, 1, config[i].gatePosY)
+	end
+end
 
 local theHiddenBeregar = Action()
 function theHiddenBeregar.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local tile, thing, thing2 , creature, lever, leverstatus
-	leverstatus = item.itemid
-	for i = 1, #config.leverPositions do
-		lever = Tile(config.leverPositions[i]):getItemById(leverstatus == 2772 and 2772 or 2773)
-		lever:transform(item.itemid == 2772 and 2773 or 2772)
+	if isInArray({32568, 32570}, item:getPosition().x) and player:getPosition().x ~= 32569 then
+		wall(1)
+	elseif isInArray({32599, 32601}, item:getPosition().x) and player:getPosition().x ~= 32600 then
+		wall(2)
 	end
-	for i = 1, #config.gatePositions do
-		tile = Tile(config.gatePositions[i])
-		if tile then
-			creature = tile:getTopCreature()
-			if leverstatus == 2772 then
-				thing = tile:getItemById(config.wallID)
-				thing2 = tile:getItemById(config.wallID2)
-				if thing and thing2 then
-					thing:remove()
-					thing2:remove()
-				end
-			else
-				Game.createItem(config.wallID, 1, config.gatePositions[i])
-				Game.createItem(config.wallID2, 1, config.gatePositions[i])
-			end
-			if creature then
-				creature:teleportTo(config.removeCreaturePosition)
-			end
-		end
-	end
-	return true
+	return item:transform(item.itemid == 2772 and 2773 or 2772)
 end
 
---theHiddenBeregar:id()
---theHiddenBeregar:register()
+theHiddenBeregar:aid(30005)
+theHiddenBeregar:register()
