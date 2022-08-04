1592c331075333f0fa17149af660ef83a3ea28da
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-08-04 09:23:49 -0300

    Add new migration for binary save

--- data/migrations/19.lua
+++ data/migrations/19.lua
@@ -1,3 +1,13 @@
 function onUpdateDatabase()
-    return false -- true = There are others migrations file | false = this is the last migration file
+	Spdlog.info("Updating database to version 20 (Binary Save), items, depot locker items,depot items and inbox items")
+	db.query("ALTER TABLE `players` ADD `items` longblob DEFAULT NULL")
+	db.query("ALTER TABLE `players` ADD `depot_locker_items` longblob DEFAULT NULL")
+	db.query("ALTER TABLE `players` ADD `depot_items` longblob DEFAULT NULL")
+	db.query("ALTER TABLE `players` ADD `inbox_items` longblob DEFAULT NULL")
+	db.query("ALTER TABLE `players` ADD `rewards` longblob DEFAULT NULL")
+	db.query("DROP TABLE `player_items`")
+	db.query("DROP TABLE `player_depotitems`")
+	db.query("DROP TABLE `player_inboxitems`")
+	db.query("DROP TABLE `player_rewards`")
+	return true
 end
