872d048746cccaa7cf742700477e62740141f126
rigis1 <rigis@o2.pl>
2022-08-13 08:47:19 +0200

    Update food.lua

--- data/scripts/actions/other/food.lua
+++ data/scripts/actions/other/food.lua
@@ -132,7 +132,7 @@ function food.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return false
 	end
 	local condition = player:getCondition(CONDITION_REGENERATION, CONDITIONID_DEFAULT)
-	if condition and math.floor(condition:getTicks() / 1000 + (itemFood[1] * 12)) >= 12000 then
+	if condition and math.floor(condition:getTicks() / 1000 + (itemFood[1] * 12)) >= 1200 then
 		player:sendTextMessage(MESSAGE_FAILURE, "You are full.")
 		return true
 	end
