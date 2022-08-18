e14b608146dd6d5f16c6208f7435e338b88269e6
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2022-08-18 08:05:15 -0300

    Fix store inbox remove items logic

--- data/modules/scripts/blessings/blessings.lua
+++ data/modules/scripts/blessings/blessings.lua
@@ -263,7 +263,7 @@ Blessings.DropLoot = function(player, corpse, chance, skulled)
 		if item then
 			local thisChance = chance
 			local thisRandom = math.random(100*multiplier)
-			if not item:isContainer() then
+			if not item:isinbox() then
 				thisChance = chance/10
 			end
 			Blessings.DebugPrint(thisChance/multiplier .. "%" .. " | thisRandom "..thisRandom/multiplier.."%", "DropLoot item "..item:getName() .. " |")
@@ -275,9 +275,22 @@ Blessings.DropLoot = function(player, corpse, chance, skulled)
 	end
 	if skulled and Blessings.Config.SkulledDeathLoseStoreItem then
 		local inbox = player:getSlotItem(CONST_SLOT_STORE_INBOX)
-		local inboxsize = inbox:getSize() - 1
-		for i = 0, inboxsize do
-			inbox:getItem(i):remove()
+		local toBeDeleted = {}
+		if inbox and inbox:getSize() > 0 then
+			for i = 0, inbox:getSize() do
+				local item = inbox:getItem(i)
+				if item then
+					toBeDeleted[#toBeDeleted + 1] = item.uid
+				end
+			end
+			if #toBeDeleted > 0 then
+				for i, v in pairs(toBeDeleted) do
+					local item = Item(v)
+					if item then
+						item:remove()
+					end
+				end
+			end
 		end
 	end
 end
