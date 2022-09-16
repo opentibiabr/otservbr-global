5480672a9eca2c2e9c61c6365f8c69b027cd3b21
LeoTK4 <leonardo.fabian@hotmail.com>
2022-09-16 18:28:14 -0300

    #Fix Error Death in Water for Field
    #Fix Error Death in Water for Field

--- data/scripts/creaturescripts/others/player_death.lua
+++ data/scripts/creaturescripts/others/player_death.lua
@@ -53,7 +53,7 @@ function playerDeath.onDeath(player, corpse, killer, mostDamageKiller, unjustifi
 	local killerLink = string.gsub(killerName, "%s+", "+")
 	local playerLink = string.gsub(playerName, "%s+", "+")
 	local serverURL = getConfigInfo("url")
-	if killer:isPlayer() then
+	if killer and killer:isPlayer() then
 		Webhook.send(playerName.." just got killed!", "**["..playerName.."]("..serverURL.."/?characters/"..playerLink..")** got killed at level " ..playerLevel.. " by **["..killerName.."]("..serverURL.."/?characters/"..killerLink..")**", WEBHOOK_COLOR_OFFLINE, announcementChannels["player-kills"])
 	else
 		Webhook.send(playerName.." has just died!", "**["..playerName.."]("..serverURL.."/?characters/"..playerLink..")** died at level " ..playerLevel.. " by " ..killerName, WEBHOOK_COLOR_WARNING, announcementChannels["player-kills"])
