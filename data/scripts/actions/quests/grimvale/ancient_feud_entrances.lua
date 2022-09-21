5c59aa2992cec0c918c3fb47f074454d75936277
Spiller <gui_spiller@hotmail.com>
2022-09-21 18:41:33 -0300

    Update ancient_feud_entrances.lua

--- data/scripts/actions/quests/grimvale/ancient_feud_entrances.lua
+++ data/scripts/actions/quests/grimvale/ancient_feud_entrances.lua
@@ -18,8 +18,8 @@ function entranceAncientFeud.onUse(creature, item, position, fromPosition)
 		return false
 	end
 	for value in pairs(config) do
-		if Position(config[value].position) == player:getPosition() then
-			player:teleportTo(Position(config.destination))
+		if Position(config[value].position) == item:getPosition() then
+			player:teleportTo(Position(config[value].destination))
 			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)		
 			return true
 		end
